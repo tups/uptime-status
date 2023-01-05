da0e0265930e577a6f09da8f0e3e1879615ee53b
Damien TUPINIER <damien.tupinier@gmail.com>
2023-01-05 14:03:17 +0100

    Branding de la page pour Beekube Mise à jour de la tooltip

--- src/components/uptimerobot.js
+++ src/components/uptimerobot.js
@@ -1,8 +1,9 @@
-import ReactTooltip from 'react-tooltip';
+
 import {useEffect, useState} from 'react';
 import {GetMonitors} from '../common/uptimerobot';
 import {formatDuration, formatNumber} from '../common/helper';
 import Link from './link';
+import {Tooltip} from "react-tooltip";
 
 function UptimeRobot({apikey}) {
 
@@ -51,13 +52,20 @@ function UptimeRobot({apikey}) {
                         status = 'down';
                         text += `Défaut ${data.down.times} fois，pendant ${formatDuration(data.down.duration)}，en ligne ${formatNumber(data.uptime)}%`;
                     }
-                    return (<i key={index} className={status} data-st={data.uptime} style={{'--average': getColor(data.uptime) }} data-tip={text}/>)
+                    return (
+                        <>
+                        <i id={`day-${index}`} key={index} className={status} data-st={data.uptime} style={{'--average': getColor(data.uptime) }} />
+                            <Tooltip key={`tooltip-day-${index}`} anchorId={`day-${index}`} className='tooltip' place='top' type='dark' effect='solid'>
+                                <div>{text}</div>
+                            </Tooltip>
+                        </>
+                    )
                 })}
             </div>
             <div className='summary'>
                 <span>{site.daily[site.daily.length - 1].date.format('YYYY-MM-DD')}</span>
             </div>
-            <ReactTooltip className='tooltip' place='top' type='dark' effect='solid'/>
+
         </div>
     ));
 
